3d85f6b259377d470d05bcae62f0b9c4b8539a7e
Felipe Garzon Melguizo <felipegarzon908@gmail.com>
2024-03-10 11:41:45 -0500

    functionality completed

---
+++ src/Person.jsx
@@ -0,0 +1,11 @@
+export const Person = ({ name, age, image }) => {
+  return (
+    <article>
+      <img src={image} alt={name} className="img" />
+      <div>
+        <h4>{name}</h4>
+        <p>{age} years</p>
+      </div>
+    </article>
+  );
+};
